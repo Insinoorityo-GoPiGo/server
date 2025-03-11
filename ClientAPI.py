5172300a87d2b08942db446b4e8b509626a9c1e4
AleksiTVT <t1koal01@students.oamk.fi>
2025-03-11 13:03:09 +0200

    add working logic for the program

--- ClientAPI.py
+++ ClientAPI.py
@@ -3,10 +3,15 @@ import threading
 from time import sleep
 from string import ascii_letters
 import json
+import asyncio
+import re
 
 class ClientAPI():
-    def __init__(self, host, port, path, default_direction="north"):
+    def __init__(self, host, port, path, quit_flag, default_direction="north"):
         
+        #Client control stuff
+        self.quit_flag = quit_flag
+
         #server stuff
         self.HOST = host
         self.PORT = port
@@ -15,8 +20,8 @@ class ClientAPI():
         self.server_socket.bind((host, port))
 
         self.server_socket.listen(1) #Allow only 1 connection
+        print("Listening for a connection.")
         self.client_socket, self.client_address = self.server_socket.accept()
-
         print(f"Connection established with {self.client_address}")
 
         #Client control stuff
@@ -33,8 +38,14 @@ class ClientAPI():
 
         #server stuff
         self.listening = True
-        self.listener_thread = threading.Thread(target=self.logic, daemon=True)
-        self.listener_thread.start()
+        #self.listener_thread = threading.Thread(target=self.logic, daemon=True)
+        #self.listener_thread.start()
+        print("__init__() done.")
+
+    def confirm(self, expected, confirmation) -> bool:
+        confirmation = re.sub(pattern='"', repl='', string=confirmation)
+        return expected == confirmation
+
 
     def check_next_node(self):
         print(self.current_node)
@@ -103,7 +114,9 @@ class ClientAPI():
         
         self.send_command(command=command) #Send a command to client to turn in the desired direction
         
-        if await self.receive_message_from_client() == "TURN_OK": #Receive a confirmation that the client has executed the turning command
+        confirmation = await self.receive_message_from_client()
+
+        if self.confirm(expected="TURN_OK", confirmation=confirmation) : #Receive a confirmation that the client has executed the turning command
             pass
         else:
             print("-----\nWrong confirmation received from client.\nIn turn_gopigo\n-----")
@@ -111,7 +124,9 @@ class ClientAPI():
     async def drive_forward(self):
         self.send_command(command="DRIVE_FORWARD") #Send a command for client to drice forward.
         
-        if await self.receive_message_from_client() == "DRIVE_OK": #Receive a confirmation from client that it has started driving forward.
+        confirmation = await self.receive_message_from_client()
+
+        if self.confirm(expected="DRIVE_OK", confirmation=confirmation): #Receive a confirmation from client that it has started driving forward.
             pass
         else:
             print("-----\nWrong confirmation received from client.\nIn turn_gopigo\n-----")
@@ -126,16 +141,26 @@ class ClientAPI():
         if self.next_node_marker <= len(self.path) - 1:
             self.next_node = self.path[self.next_node_marker]
 
-    def logic(self):
+    async def logic(self):
+        print("In ClientAPI.logic(), before while")
         while self.listening:
-            self.logic_loop()
-            self.drive_back()
+            print("First path driving")
+            await self.logic_loop()
+            print("Second path driving")
+            await self.drive_back()
+            print("Done driving.")
+            self.quit_flag.set()
+            self.close_connection()
 
     async def logic_loop(self):
         if self.current_node_marker == 0:
             self.send_command(command="ARE_YOU_READY") #Send a check to client to make sure it's ready
-            
-            if await self.receive_message_from_client() == "I_AM_READY": #Receive a ready confirmation from client
+            print("In logic_loop, after sending the command")
+
+            confirmation = await self.receive_message_from_client() #This is where the error comes from.
+
+            if self.confirm(expected="I_AM_READY", confirmation=confirmation): #Receive a ready confirmation from client
+                print("In logic_loop, response has been received")
                 pass
             else:
                 print("-----\nWrong confirmation received from client.\nIn turn_gopigo\n-----")
@@ -154,10 +179,10 @@ class ClientAPI():
 
             if self.is_gopigo_facing_next_node(cardinal_direction=cardinal_direction):
                 print("GoPiGo is facing the next node.")
-                self.drive_forward()
+                await self.drive_forward()
             else:
-                self.turn_gopigo(where_from=self.gopigo_direction, to_where=cardinal_direction)
-                self.drive_forward()
+                await self.turn_gopigo(where_from=self.gopigo_direction, to_where=cardinal_direction)
+                await self.drive_forward()
 
     def reverse_path(self):
         self.path = list(reversed(self.path))
@@ -169,15 +194,17 @@ class ClientAPI():
         self.current_node = self.path[self.current_node_marker]
         self.next_node = self.path[self.next_node_marker]
 
-    def drive_back(self):
+    async def drive_back(self):
         self.reverse_path()
         self.reset_node_markers()
-        self.logic_loop()
+        await self.logic_loop()
 
     async def receive_message_from_client(self):
+        print("In receive_message_from_client, before try")
         try:
             if self.listening:
-                message = await self.client_socket.recv(1024).decode()
+                message = self.client_socket.recv(1024).decode()
+                print("In receive_message_from_client, after receiving message from client")
                 print(f"Client: {message}")
 
                 if not message: #Stop if client disconnects, make better: client_disconnected() listening = False
@@ -192,6 +219,8 @@ class ClientAPI():
     def send_command(self, command: str):
         command_type = None
 
+        print("In send_command, the command: ", command)
+
         if command == "TURN_RIGHT" or command == "TURN_TWICE_RIGHT" or command == "TURN_LEFT" or command == "DRIVE_FORWARD":
             command_type = "COMMAND"
         elif command == "ARE_YOU_READY":
@@ -205,6 +234,7 @@ class ClientAPI():
         )
 
         try:
+            print("In try")
             self.client_socket.sendall(command_json.encode())
         except Exception as e:
             print(f"Error sending command: {e}") #Maybe some error handling here
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from threading import Event
+from time import sleep
+import asyncio
+
 from PathFinding import PathFinding
 from ClientAPI import ClientAPI
 
-def main():
+quit_flag = Event()
+
+loop = asyncio.get_event_loop()
+
+async def main():
     path = PathFinding().get_shortest_path(start="A1", end="F9")
-    client_api = ClientAPI(host="127.0.0.1", port=1025, path=path)
+    client_api = ClientAPI(host="127.0.0.1", port=1025, path=path, quit_flag=quit_flag)
+
+    #loop.run_until_complete(future=client_api.logic())
+    print("Before await client_api.logic()")
+    await client_api.logic()
+    print("After await client_api.logic()")
+
+    while True:
+        sleep(0.5)
+        if quit_flag.is_set():
+            break
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
