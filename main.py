17241091337d1063232698c7556f9eb4a7cfbb5b
AleksiTVT <t1koal01@students.oamk.fi>
2025-03-14 10:01:45 +0200

    add threading to socket connection

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from threading import Event
+import threading
 from time import sleep
 import asyncio
 
 from PathFinding import PathFinding
 from ClientAPI import ClientAPI
 
-quit_flag = Event()
+quit_flag = threading.Event()
 
-async def main():
+async def run_logic(client_api):
+    print("In run_logic")
+    await client_api.logic()
+
+def run_function(client_api):
+    print("In run_function")
+    asyncio.run(run_logic(client_api=client_api))
+
+def start_thread(client_api):
+    print("In startt_thread")
+    (threading.Thread(target=run_function, args=(client_api,))).start()
+
+
+
+def main():
     path = PathFinding().get_shortest_path(start="A1", end="F9")
     client_api = ClientAPI(host="127.0.0.1", port=1025, path=path, quit_flag=quit_flag)
 
     #loop.run_until_complete(future=client_api.logic())
     print("Before await client_api.logic()")
-    await client_api.logic()
+    start_thread(client_api=client_api)
     print("After await client_api.logic()")
 
     while True:
@@ -22,4 +36,4 @@ async def main():
             break
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
