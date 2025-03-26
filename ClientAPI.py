efbc950cce1b388318dbf6c7b6e63c7f40e0d58c
Saku Suorajärvi <saku.suorajarvi@hotmail.fi>
2025-03-26 13:46:06 +0200

    Working multiple client threads

--- ClientAPI.py
+++ ClientAPI.py
@@ -37,9 +37,9 @@ class ClientAPI():
 
     async def open_connection(self):
         self.server_socket.listen(1) #Allow only 1 connection
-        print(f"[{self.bot_id}]Listening for a connection.")
+        print("Listening for a connection.")
         self.client_socket, self.client_address = self.server_socket.accept()
-        print(f"[{self.bot_id}]Connection established with {self.client_address}")
+        print(f"Connection established with {self.client_address}")
         
         self.listening = True
         print("Connection opened.")
@@ -119,17 +119,17 @@ class ClientAPI():
         
         confirmation = self.receive_message_from_client()
 
-        if self.confirm(expected=f"[{self.bot_id}]TURN_OK", confirmation=confirmation) : #Receive a confirmation that the client has executed the turning command
+        if self.confirm(expected="TURN_OK", confirmation=confirmation) : #Receive a confirmation that the client has executed the turning command
             pass
         else:
             print("-----\nWrong confirmation received from client.\nIn turn_gopigo\n-----")
 
     def drive_forward(self):
-        self.send_command(command=f"[{self.bot_id}]DRIVE_FORWARD") #Send a command for client to drice forward.
+        self.send_command(command="DRIVE_FORWARD") #Send a command for client to drice forward.
         
         confirmation = self.receive_message_from_client()
 
-        if self.confirm(expected=f"[{self.bot_id}]DRIVE_OK", confirmation=confirmation): #Receive a confirmation from client that it has started driving forward.
+        if self.confirm(expected="DRIVE_OK", confirmation=confirmation): #Receive a confirmation from client that it has started driving forward.
             pass
         else:
             print("-----\nWrong confirmation received from client.\nIn turn_gopigo\n-----")
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run_server(client_api):
 
 def start_thread(client_api):
     print("In start_thread")
-    (threading.Thread(target=run_server, args=(client_api,), daemon=True)).start()
+    (threading.Thread(target=run_server, args=(client_api,), daemon=False)).start()
 
 
 
@@ -28,13 +28,13 @@ def main():
     path2 = PathFinding().get_shortest_path(start="G5", end="D1")
     map = Map(queue=location_queue, quit_flag=quit_flag)
     
-    client_api_1 = ClientAPI(host="127.0.0.1", port=1025, path=path1, quit_flag=quit_flag, location_queue=location_queue, default_direction="East", bot_id="Bot_1")
-    client_api_2 = ClientAPI(host="127.0.0.1", port=1026, path=path2, quit_flag=quit_flag, location_queue=location_queue, default_direction="West", bot_id="Bot_2")
+    client_api_1 = ClientAPI(host="192.168.34.87", port=1025, path=path1, quit_flag=quit_flag, location_queue=location_queue, default_direction="east", bot_id="Bot_1")
+    client_api_2 = ClientAPI(host="192.168.34.87", port=1026, path=path2, quit_flag=quit_flag, location_queue=location_queue, default_direction="west", bot_id="Bot_2")
     start_thread(client_api=client_api_1)
     start_thread(client_api=client_api_2)
 
     print("before map.run()")
-    map.run()
+    #map.run()
 
 if __name__ == "__main__":
     main()
