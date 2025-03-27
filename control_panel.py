6884dd9234611751a010259d57222737732f1299
AleksiTVT <t1koal01@students.oamk.fi>
2025-03-27 12:07:31 +0200

    add manual setting for path and socket starting into control panel

--- control_panel.py
+++ control_panel.py
@@ -1,13 +1,14 @@
 from tkinter import *
 import threading
-from tkinter.ttk import Combobox 
-
+from tkinter.ttk import Combobox
+import asyncio
 
 from map import Map
-
+from PathFinding import PathFinding
+from ClientAPI import ClientAPI
 
 class Control_Panel:
-    def __init__(self, command_queue, location_queue, quit_flag,):
+    def __init__(self, command_queue, location_queue, quit_flag):
         self.command_queue = command_queue
         self.valid_inputs = [
             "A0" ,
@@ -24,10 +25,9 @@ class Control_Panel:
             "G1", "G2", "G3", "G4",
             "G5", "G6", "G7", "G8", "G9"
         ]
-        
-        
-         
 
+        self.path: None|list = None
+        
         self.location_queue = location_queue
         self.quit_flag = quit_flag
 
@@ -38,9 +38,7 @@ class Control_Panel:
         self.app = Tk()
         self.app.title("Control Panel")
         self.app.geometry("600x400")
-        
 
-        # Button 1
         self.b1 = Button(self.app, text="Start GPG1", command=lambda: self.handle_button_press("GPG1"))
         self.b1.grid(row=0, column=0, padx=10, pady=10)
 
@@ -59,16 +57,12 @@ class Control_Panel:
         self.create_node_fields_gpg1()
         self.create_node_fields_gpg2()
   
-
         self.app.bind("<Escape>", self.close_app)
 
     def close_app(self, event):
-        
         self.app.destroy() #Close the control panel window
-
      
     def create_node_fields_gpg1(self):
-       
         aloitus_label_1 = Label(self.app, text='Aloitus 1', font=('Arial', 10, 'bold'))
         aloitus_label_1.grid(row=0, column=10, padx=10, pady=5)
         
@@ -87,9 +81,7 @@ class Control_Panel:
         self.separator = Frame(self.app, height=2, bd=1, relief=SUNKEN)
         self.separator.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
         
-        
     def create_node_fields_gpg2(self):
-        
         aloitus_label_2 = Label(self.app, text='Aloitus 2', font=('Arial', 10, 'bold'))
         aloitus_label_2.grid(row=60, column=10, padx=10, pady=5)
         
@@ -106,17 +98,17 @@ class Control_Panel:
         sub_btn_2.grid(row=61, column=12, pady=7)
         
     def submit_gpg1(self):
-        
         Aloitus1 = self.start_node_var_1.get()
         Lopetus1 = self.end_node_var_1.get()
 
         print(f"Aloitus Node GPG1: {Aloitus1}")
         print(f"Lopetus Node GPG1: {Lopetus1}")
         
+        self.path = PathFinding().get_shortest_path(start=Aloitus1, end=Lopetus1)
+
         self.end_node_var_1.set("")
         
     def submit_gpg2(self):
-        
         Aloitus2 = self.start_node_var_2.get()
         Lopetus2 = self.end_node_var_2.get()
 
@@ -124,7 +116,6 @@ class Control_Panel:
         print(f"Lopetus Node GPG2: {Lopetus2}")
         
         self.end_node_var_2.set("")
-        
 
     def open_control_panel(self):
         self.app.mainloop()
@@ -133,6 +124,11 @@ class Control_Panel:
         print("Open map button pressed.")
         self.location_map = Map(queue=self.location_queue, quit_flag=self.quit_flag, master=self.app)
         (threading.Thread(target=self.location_map.run, daemon=True)).start()
+
+    def open_and_run_socket(self):
+        client_api = ClientAPI(host="127.0.0.1", port=1025, path=self.path, quit_flag=self.quit_flag, command_queue=self.command_queue, location_queue=self.location_queue, default_direction="east", bot_id="Bot_1")
+        run_server = lambda client_api: asyncio.run(client_api.open_connection())
+        (threading.Thread(target=run_server, args=(client_api,), daemon=True)).start()
 
     def handle_button_press(self, command):
         print(f"Button clicked: {command}")
@@ -149,3 +145,5 @@ class Control_Panel:
         elif command == "open_map":
             print("open_map command received.")
             self.open_map()
+        elif command == "GPG1":
+            self.open_and_run_socket()
--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 import os
 
 from map import Map
-from PathFinding import PathFinding
-from ClientAPI import ClientAPI
+
 from control_panel import Control_Panel
 
 load_dotenv()
@@ -26,15 +25,14 @@ def start_thread(client_api):
 
 
 def main():
-    path_1 = PathFinding().get_shortest_path(start="A0", end="E5")
-    path_2 = PathFinding().get_shortest_path(start="G5", end="D1")
-    #location_map = Map(queue=location_queue, quit_flag=quit_flag)
+    #path_1 = PathFinding().get_shortest_path(start="A0", end="E5")
+    #path_2 = PathFinding().get_shortest_path(start="G5", end="D1")
     
     #os.getenv("IP_ADDRESS")
-    client_api_1 = ClientAPI(host="127.0.0.1", port=1025, path=path_1, quit_flag=quit_flag, command_queue=command_queue, location_queue=location_queue, default_direction="east", bot_id="Bot_1")
-    client_api_2 = ClientAPI(host="127.0.0.1", port=1026, path=path_2, quit_flag=quit_flag, command_queue=command_queue, location_queue=location_queue, default_direction="west", bot_id="Bot_2")
-    start_thread(client_api=client_api_1)
-    start_thread(client_api=client_api_2)
+    #client_api_1 = ClientAPI(host="127.0.0.1", port=1025, path=path_1, quit_flag=quit_flag, command_queue=command_queue, location_queue=location_queue, default_direction="east", bot_id="Bot_1")
+    #client_api_2 = ClientAPI(host="127.0.0.1", port=1026, path=path_2, quit_flag=quit_flag, command_queue=command_queue, location_queue=location_queue, default_direction="west", bot_id="Bot_2")
+    #start_thread(client_api=client_api_1)
+    #start_thread(client_api=client_api_2)
 
     control_panel=Control_Panel(command_queue=command_queue, location_queue=location_queue, quit_flag=quit_flag)
 
