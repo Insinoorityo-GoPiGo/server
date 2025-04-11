dabbf9898c733a7ec4d80e1da2c76af0c917852e
AleksiTVT <t1koal01@students.oamk.fi>
2025-04-11 12:42:55 +0300

    improve the logic in ImageAnalysisHander

--- ImageAnalysisHandler.py
+++ ImageAnalysisHandler.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import threading
 
 from ImageReceiverSocket import ImageReceiverSocket
 from OpenaiAPI import OpenaiAPI
@@ -7,14 +8,18 @@ from OpenaiAPI import OpenaiAPI
 load_dotenv()
 
 class ImageAnalysisHander:
-    def __init__(self, ):
+    def __init__(self):
         self.image_receiver_socket = ImageReceiverSocket(host=os.environ.get("IP_ADDRESS"), port=1100)
         self.openai_api = OpenaiAPI(api_key=os.environ.get("API_KEY"))
 
     def start(self):
         self.image_receiver_socket.open_connection()
 
-    def logic(self):
-        image_as_bytes = self.image_receiver_socket.receive_image()
-        description = self.openai_api.get_response(image_data=image_as_bytes) #description of the object
-        #Here send the description to a queue
+    def logic(self):             
+        threading.Thread(target=self.logic_loop, daemon=True).start()
+        
+    def logic_loop(self):
+        while True:
+            image_as_bytes = self.image_receiver_socket.receive_image()
+            description = self.openai_api.get_response(image_data=image_as_bytes) #description of the object
+            #Here send the description to a queue
