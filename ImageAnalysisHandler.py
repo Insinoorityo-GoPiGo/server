b633788c95410c099a09d0c683ae1e60f484ffc4
AleksiTVT <t1koal01@students.oamk.fi>
2025-04-11 12:32:11 +0300

    add start and logic functions to ImageAnalysisHander

--- ImageAnalysisHandler.py
+++ ImageAnalysisHandler.py
@@ -7,8 +7,14 @@ from OpenaiAPI import OpenaiAPI
 load_dotenv()
 
 class ImageAnalysisHander:
-    def __init__(self):
+    def __init__(self, ):
         self.image_receiver_socket = ImageReceiverSocket(host=os.environ.get("IP_ADDRESS"), port=1100)
         self.openai_api = OpenaiAPI(api_key=os.environ.get("API_KEY"))
 
-    
+    def start(self):
+        self.image_receiver_socket.open_connection()
+
+    def logic(self):
+        image_as_bytes = self.image_receiver_socket.receive_image()
+        description = self.openai_api.get_response(image_data=image_as_bytes) #description of the object
+        #Here send the description to a queue
