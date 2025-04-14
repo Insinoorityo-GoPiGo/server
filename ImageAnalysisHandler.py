3a2900ee53442651c70103d741c78e0572fa64f4
AleksiTVT <t1koal01@students.oamk.fi>
2025-04-14 11:31:55 +0300

    commented lines for testing purposes

--- ImageAnalysisHandler.py
+++ ImageAnalysisHandler.py
@@ -10,7 +10,7 @@ load_dotenv()
 class ImageAnalysisHander:
     def __init__(self, obstacle_description_queue):
         self.image_receiver_socket = ImageReceiverSocket(host=os.environ.get("IP_ADDRESS"), port=1100)
-        self.openai_api = OpenaiAPI(api_key=os.environ.get("API_KEY"))
+        #self.openai_api = OpenaiAPI(api_key=os.environ.get("API_KEY"))
 
         self.obstacle_description_queue = obstacle_description_queue
 
@@ -21,5 +21,5 @@ class ImageAnalysisHander:
     def logic_loop(self):
         while True:
             image_as_bytes = self.image_receiver_socket.receive_image()
-            description = self.openai_api.get_response(image_data=image_as_bytes) #description of the object
-            self.obstacle_description_queue.put(description)
+            #description = self.openai_api.get_response(image_data=image_as_bytes) #description of the object
+            #self.obstacle_description_queue.put(description)
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from queue import Queue
 def main():
     #Openai ja kuvankäsittely -threadin käynnistys
     obstacle_description_queue = Queue()
-    #image_analysis_handler = ImageAnalysisHander(obstacle_description_queue=obstacle_description_queue)
-    #image_analysis_handler.start()
+    image_analysis_handler = ImageAnalysisHander(obstacle_description_queue=obstacle_description_queue)
+    image_analysis_handler.start()
 
     PathFinding.COORDINATES, PathFinding.EDGES = get_coordinates_and_edges()
 
